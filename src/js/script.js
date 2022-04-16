c3b03fb96970edd23b2c9c55d72fbbb4804c0dd1
khobotovdaniil <dinich44@gmail.com>
2022-04-16 15:19:14 +0300

    added slick  slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+    });
+});
