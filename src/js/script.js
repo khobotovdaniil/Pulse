591f62874b13a92e46fb1aa6524f731ea9f95bf8
khobotovdaniil <dinich44@gmail.com>
2022-04-18 00:29:14 +0300

    added slider mobile adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                arrows: false,
+                dots: true,
+              }
+            },
+        ]
     });
 });
