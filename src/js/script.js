de0c810f7d199ffc109abe92d0028c6769733684
khobotovdaniil <dinich44@gmail.com>
2022-04-19 21:08:00 +0300

    added forms validation

--- src/js/script.js
+++ src/js/script.js
@@ -52,4 +52,30 @@ $(document).ready(function(){
     })
   });
 
+    //validation
+  function validateForms(form){
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Пожалуйста, введите свое имя",
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
 });
