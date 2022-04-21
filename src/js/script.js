3d85f16e985df3a29f1f274c039dfd34c8c76da3
khobotovdaniil <dinich44@gmail.com>
2022-04-21 16:52:36 +0300

    added smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -99,4 +99,17 @@ $(document).ready(function(){
     };    
   });
   
+    //Smooth scroll and pageup
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1200) {
+      $('.pageup').fadeIn();
+    } else $('.pageup').fadeOut();
+  });
+
+  $("a[href^='#']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;  
+  });
+
 });
