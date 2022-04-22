a86ea46f1066fd8636cb17b4c9a404c62cbbbff6
khobotovdaniil <dinich44@gmail.com>
2022-04-22 11:25:02 +0300

    html validation

--- src/js/script.js
+++ src/js/script.js
@@ -106,7 +106,7 @@ $(document).ready(function(){
     } else $('.pageup').fadeOut();
   });
 
-  $("a[href^='#']").click(function() {
+  $("a[href^='#up']").click(function() {
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;  
